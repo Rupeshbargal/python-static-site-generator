e450d9ca1e09a19c4f18714febb676bd0f5cd8f5
Rupeshbargal <115457464+Rupeshbargal@users.noreply.github.com>
2022-10-12 13:24:32 +0530

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -4,10 +4,11 @@ from ssg.site import Site
 import ssg.parsers
 
 def main(source="content", dest="dist"):
-    config = {"source": source,
-              "dest": dest,
-              "parser": [ssg.parsers.ResourceParser(),],
-              }
+    config = {
+        "source": source,
+        "dest": dest,
+        "parser": [ssg.parsers.ResourceParser(),],
+    }
     Site(**config).build()
 
     
